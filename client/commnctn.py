2e4b7bb586d35e4262b352c756aa7c2a86a54434
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2025-01-22 19:03:28 +0100

    Update commnctn.py

--- client/commnctn.py
+++ client/commnctn.py
@@ -1,51 +1,37 @@
 import serial
+import time
+
+BAUDRATE = 115200
 
 class Communication:
-    __port: str
-    __speed: int
+     def __init__(self, port, baudrate=BAUDRATE):
+         self.ser = serial.Serial(port, baudrate, timeout=1)
+      
+     def communication_send(self, buffer: bytes):
+        if not self.ser.is_open:
+            raise Exception("Serial connection is not open.")
+        return self.ser.write(buffer)
+    
+     def communication_read(self, size: int) -> bytes:
+        if not self.ser.is_open:
+            raise Exception("Serial connection is not open.")
+        return self.ser.read(size)
+    
+    
+     def communication_open(self) -> bool:
+        if not self.ser.is_open:
+            self.ser.open()
+        return self.ser.is_open
+    
+    
+     def communication_close(self):   
+        if self.ser.is_open:
+            self.ser.close()
+            
+     # Test Code
+if __name__ == "__main__":
+    print("Communication module is running successfully!")
+            
+  
 
-    def __init__(self, info: str) -> None:
-        addr = info.split(" ")
-        Communication.__port = addr[0]
-        Communication.__speed = int(addr[1])
-        self.__con = serial.Serial(Communication.__port, Communication.__speed)
-
-    def connect(self) -> bool:
-        status = False
-        try:
-            if not self.__con.is_open:
-                self.__con.open()
-            status = self.__con.is_open
-        except:
-            pass
-        return status
-
-    def disconnect(self):
-        try:
-            self.__con.close()
-        except:
-            pass
-
-    def send(self, buf: bytes) -> bool:
-        status = False
-        try:
-            if self.__con.is_open:
-                self.__con.reset_output_buffer()
-                status = (len(buf) == self.__con.write(buf))
-        except:
-            pass
-        return status
-
-    def receive(self, size: int) -> bytes:
-        buffer = b""
-        try:
-            if self.__con.is_open:
-                self.__con.reset_input_buffer()
-                buffer = self.__con.read(size)
-        except:
-            pass
-        return buffer
-
-    def __del__(self):
-        self.disconnect()
   

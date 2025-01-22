5f43dd993ed7c206151ea50c33631ca40da674ff
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2025-01-22 19:05:42 +0100

    Update commnctn.h

--- server/lib/commnctn/commnctn.h
+++ server/lib/commnctn/commnctn.h
@@ -5,7 +5,7 @@
 #include <stddef.h>
 
 bool communication_init(void);
-bool communication_send(const uint8_t *data, size_t dlen);
-size_t communication_receive(uint8_t *buf, size_t blen);
+bool communication_write(const uint8_t *data, size_t dlen);
+size_t communication_read(uint8_t *buf, size_t blen);
 
 #endif /* COMMNCTN_H */
